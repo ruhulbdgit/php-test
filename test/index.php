3e01bc114d57218ab173ba788ab861c04bcefbed
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2024-12-25 14:40:10 +0600

    just test inheritance
    from w3 school test with inheritance

--- test/index.php
+++ test/index.php
@@ -101,7 +101,7 @@ if (isset($_COOKIE['user_id'])) {
         }
         function __destruct()
         {
-            echo 'HI, I am from Destruct Fucntion';
+            // echo 'HI, I am from Destruct Fucntion';
         }
     }
     //1st instans of Car class
@@ -117,6 +117,35 @@ if (isset($_COOKIE['user_id'])) {
     echo "</br>";
     echo $cars2->get_color();
     echo '</br>';
+    echo "<hr>";
+    //Oop with inheritance
+    class Fruit
+    {
+        public $name;
+        public $color;
+        public function __construct($name, $color)
+        {
+            $this->name = $name;
+            $this->color = $color;
+        }
+        public function intro()
+        {
+            echo "The fruit is {$this->name} and the color is {$this->color}.";
+        }
+    }
+
+    // Strawberry is inherited from Fruit
+    class Strawberry extends Fruit
+    {
+        public function message()
+        {
+            echo "Am I a fruit or a berry? ";
+        }
+    }
+
+    $strawberry = new Strawberry("Strawberry", "red");
+    $strawberry->message();
+    $strawberry->intro();
     ?>
 
 
